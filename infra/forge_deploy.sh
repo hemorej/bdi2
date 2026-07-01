2b2a485117cfeaa02a6b28be38d2e98d261012b5
j_a_ <jerome.arfouche@pm.me>
2026-07-01 14:43:55 -0400

    style fix, deployment fix

--- infra/forge_deploy.sh
+++ infra/forge_deploy.sh
@@ -2,9 +2,11 @@ $CREATE_RELEASE()
 
 cd $FORGE_RELEASE_DIRECTORY
 
-npm install
+npm install 
 npm run build
 npm prune --omit=dev
-ln -s /mnt/volume_tor1_01/bdi2-results results
+
+ln -s /mnt/volume-tor1-01/bdi2-results results
+ln -s /mnt/volume-tor1-01/bdi2fonts public/fonts
 
 $ACTIVATE_RELEASE()
